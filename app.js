b8448d287425e257d09c7cb30dc21afed8d0df88
Shelby Gluck <shelbygluck@gmail.com>
2019-11-14 12:06:37 -0500

    successful connection

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ app.use(morgan("dev"));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send('hello world');
+
+app.get('/', (req, res) => {
+  console.log('on page');
+  res.send('<h1>homepage</h1>');
 });
 
-app.listen(2021, () => {
+const PORT = 2021;
+
+app.listen(PORT, () => {
     console.log(`app is listening on port 2021`);
 });
