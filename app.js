5d670e004b73f72b3ac211826f03ea1cd414c312
Laura Altia Barger <laura.altia@gmail.com>
2019-11-14 12:22:20 -0500

    added public folder with stylesheet

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
+const layout = require('./views/layout.js');
 const app = express();
 
 app.use(morgan("dev"));
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   console.log('on page');
-  res.send('<h1>homepage</h1>');
+  res.send(layout('smile'));
 });
 
 const PORT = 2021;
